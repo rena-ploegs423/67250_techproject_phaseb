3b26d81d2ef74be82e82cdc6bb69de191905cbba
rena-ploegs423 <sfploeger@gmail.com>
2026-04-05 18:41:54 -0400

    JavaScript  asics (console practice)

---
+++ sploeger-increment3/static/script.js
@@ -0,0 +1,61 @@
+// Numeric variables and addition operation
+var x = 5
+var y = 7
+var z = x + y
+console.log(z)
+
+// String variables and concatenation operation
+A = "Hello "
+B = "world!"
+C = A + B
+console.log(C)
+
+// sumnPrint definition
+function sumnPrint(x1, x2) {
+    var result = x1 + x2
+    console.log(result)
+}
+
+// sumnPrint call
+sumnPrint(x, y);
+sumnPrint(A, B);
+
+// Conditional statement
+if (C.length > z) {
+    console.log(C)
+    if (C.length < z) {
+        console.log(z)
+    }
+} else {
+    console.log("good job!")
+}
+
+// Array creation
+L1 = ["Watermelon","Pineapple","Pear","Banana"];
+L2 = ["Apple","Banana","Kiwi","Orange"];
+
+// for loop function to find the banana in arrays
+// function findTheBanana(arr) {
+//    for (var i = 0; i < arr.length; i++) {
+//        if (arr[i] === "Banana") {
+//            alert("Found the banana!");
+//        }
+//    }
+// } 
+
+// Call function in both arrays
+// findTheBanana(L1);
+// findTheBanana(L2);
+
+// forEach function to find the banana in arrays
+// function findTheBanana(arr) {
+//     arr.forEach(function(item) {
+//         if (item === "Banana") {
+//             alert("Found the banana!");
+//         }
+//     });
+// }
+
+// Call function in both arrays
+// findTheBanana(L1);
+// findTheBanana(L2);
