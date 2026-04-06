f3792df21ecd16e9ac8dc6b3af7570d54d62310d
rena-ploegs423 <sfploeger@gmail.com>
2026-04-05 22:41:04 -0400

    “read more / read less” toggle using jQuery (index.html)

--- sploeger-increment4/static/script.js
+++ sploeger-increment4/static/script.js
@@ -90,11 +90,11 @@ function addYear() {
 addYear();
 
 
-/* Sets the 'active' class on the navigation link that matches the current page URL.*/
+// Sets the 'active' class on the navigation link that matches the current page URL.//
 
 function ActiveNav() {
     // Get all navigation links
-    const navLinks = document.querySelectorAll('nav li a');
+    const navLinks = document.querySelectorAll('nav a');
 
     // Iterate over each link
     navLinks.forEach(link => {
@@ -108,3 +108,18 @@ function ActiveNav() {
 
 // Execute the function to set the active navigation link on page load
 ActiveNav();
+
+
+ // When the "Read Less" button is clicked
+ $("#readLess").click(function(){ 
+    $("#longIntro").hide(); // Hide the long introduction text
+    $("#readLess").hide();  // Hide the "Read Less" button itself
+    $("#readMore").show();  // Show the "Read More" button  
+  });
+  
+// When the "Read More" button is clicked
+  $("#readMore").click(function(){
+    $("#longIntro").show();  // Show the long introduction text
+    $("#readLess").show();   // Show the "Read Less" button
+    $("#readMore").hide();   // Hide the "Read More" button  
+  });
